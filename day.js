22fc67f8d48233f810d1ffc9518651510c78d761
Dean Palm <dean@sxsw.com>
2018-08-17 00:07:25 -0500

    now from a file

---
+++ day.js
@@ -0,0 +1,4 @@
+function dayOfTheWeek(date) {
+    const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday"]; return daysOfTheWeek[date.getDay()];
+  }
